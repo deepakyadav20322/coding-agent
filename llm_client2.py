67137ecd02933b9044f60d942a6c7f17b6aacec5
deepak <informationtechnology20322@gmail.com>
2026-02-01 22:17:00 +0530

    handle non-streamming response by tool

--- llm_client2.py
+++ llm_client2.py
@@ -1,6 +1,7 @@
 # ========================================
-# ==============================Some usefull point =================
-# Autoregressive => the model generates text one token at a time, and each new token is predicted using all the previous tokens.
+# ==============================Some usefull point 👇👇👇👇=================
+#💀 Autoregressive => the model generates text one token at a time, and each new token is predicted using all the previous tokens.
+# 💀 what is coroutene in python
 
 
 # =========================
@@ -16,12 +17,13 @@ class LLMClient:
     def get_client(self)->AsyncOpenAI:
         if self._client is None:
             self._client = AsyncOpenAI(
-                api_key = "9834732hfjdshfkjhfkjdshfkj",
-                base_url="https://api.openrouter.com/v1",
+                api_key = "sdkjsfkdshfkjsdhfkjshfkjhskjf-testing",
+                base_url="https://openrouter.ai/api/v1",
                 # here I don't add model name because I want to set it dynamically while making requests and also add auto model selct logic based on request which cursir does
                 # model="",
 
             )
+        return self._client
 
     # close the already initiated client 
     async def close(self)->None:
@@ -31,18 +33,33 @@ class LLMClient:
 
     async def chat_completion(self,message:list[dict[str,Any]],stream:bool=True):
 
+        client = self.get_client()
+        kwargs = {
+            "model":"nvidia/nemotron-3-nano-30b-a3b:free",
+            "messages":message,
+            "stream":stream
+        }
+
         # These are two different methods to handle streaming and non-streaming responses it is private methods 
         if stream:
-            self._stream_response
+            await self._stream_response()
         else:
-            self._non_stream_response
+            await self._non_stream_response(client,kwargs)
 
 
 #  PRIVTAE METHODS TO GET RESPONSES
     async def _stream_response(self):
         pass
-    async def _non_stream_response(self):
-        pass
+    async def _non_stream_response(self,client:AsyncOpenAI,kwargs:dict[str,Any]):
+        response  = await client.chat.completions.create(**kwargs)
+        message = response.choices[0].message
+
+        text = None
+        if message.content:
+            pass 
+        # 😊"=😊"😊"😊"
+
+        print(response)
 
 
         
---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+import asyncio
+from llm_client2 import LLMClient
+
+
+
+async def main():
+    llm_client = LLMClient()
+    message=[
+            {"role":"user","content":"Hello, how are you?"  }
+        ]
+    await llm_client.chat_completion(
+      message,
+        False
+    )
+    print('done..')
+
+# This runs the async function properly
+if __name__ == "__main__":
+    result = asyncio.run(main())
+    # print(f"Response: {result}")
