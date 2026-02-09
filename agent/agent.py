8d62ae9c10f25311188f75c2af5ade74040ef10b
deepak <informationtechnology20322@gmail.com>
2026-02-09 23:13:06 +0530

    add Agent message and show message content TUI way

--- agent/agent.py
+++ agent/agent.py
@@ -2,13 +2,48 @@
 
 from typing import AsyncGenerator
 
-from agent.events import AgentEvent
+from agent.events import AgentEvent, AgentEventType
+from client.llm_client import LLMClient
+from client.response import StreamEventType
 
 
 class Agent:
     def __init__(self):
-        pass
+        self.client = LLMClient()
+
+        async def run(self, message:str):
+            yield AgentEvent.agent_start(message)
+#   Add user message to the context and send it to the llm client and get the response as stream of events and then convert those events into agent event and yield it to the caller of this function
+
+            async for event in self._agentic_loop():
+                yield event
+
+                if event.type == AgentEventType.TEXT_COMPLETE:
+                    final_response = event.data.get("content")
+            
+            yield AgentEvent.agent_end(final_response)
 
         # Agentic loop is multi turn conversation which come letter
-    async def agentic_loop()->AsyncGenerator[AgentEvent,None]:
-        pass
+        async def _agentic_loop()->AsyncGenerator[AgentEvent,None]:
+            messages=[{"role":"user","content":"Hello, how are you?"  }]
+
+            response_text = ""
+            async for event in self.client.chat_completion(  messages, True ):
+                if event.type == StreamEventType.TEXT_DELTA:
+                    content  = event.text_delta.content 
+                    yield AgentEvent.text_delta(content)
+
+                elif event.type == StreamEventType.ERROR:
+                    yield AgentEvent.agent_error(event.error or "Unknown error occured")
+                    # return # Here if you want terminate the excuetion of agent b/c we get error but more you do handle this
+
+            if response_text:
+                yield AgentEvent.text_complete(response_text)
+    
+    async def __aenter__(self):
+        return self
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self.client:
+            await self.client.close()
+
+               
--- main.py
+++ main.py
@@ -32,34 +32,52 @@
 
 import asyncio
 from typing import Any
+from agent.agent import Agent
+from agent.events import AgentEventType
 from client.llm_client2 import LLMClient
 import click
 
 class CLI:
     def __init__(self):
-        pass
-    def run_single(self):
-         pass
+        self.agent = Agent | None = None
+    async def run_single(self,message:str):
+         async with Agent() as agent:
+             self.agent = agent
+             self._process_message(message)
+
+    # This function is responsible for processing the user message and getting the response from the agent and print it to the according to the type of event and message type we get from the agent
+    async def _process_message(self,message:str)->str | None:
+        if not self.agent:
+            return None
+        async for event in self.run_single(message):
+            if event.type == AgentEventType.AGENT_START:
+                # print(f"Agent started with message: {event.data.get('message')}")
+                return
+            if event.type == AgentEventType.TEXT_DELTA:
+                content = event.data.get("content","")
 
 
 
-async def run(message:dict[str,Any]):
-     client = LLMClient()
-     async for event in client.chat_completion(
-      message,
-        True
-    ):
-             print(event)
+
+
+# We remove it from here and put it into agent.py
+# async def run(message:dict[str,Any]):
+#      client = LLMClient()
+#      async for event in client.chat_completion(
+#       message,
+#         True
+#     ):
+#              print(event)
 
 
 @click.command()
 @click.argument("prompt",required=False)
-async def main():
-    messages=[
-            {"role":"user","content":"Hello, how are you?"  }
-        ]
-    asyncio.run(run(messages))
-    print('done..')
+async def main(prompt:str|None):
+    cli = CLI()
+    # messages=[
+    #         {"role":"user","content":prompt}
+    #     ]
+    asyncio.run(cli.run_single(prompt))
 
 # This runs the async function properly
 if __name__ == "__main__":
